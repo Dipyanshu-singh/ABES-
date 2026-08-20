e072fd9165d5d82e93e2d6f3d66b84bbea5b9bbe
SHRED <shred@SHREDs-MacBook-Pro.local>
2026-08-20 15:21:23 +0530

    pq_minimum

---
+++ ADS/minimum_PQ.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+int main(){
+    priority_queue<int,vector<int>,greater<int>> pq;
+    
+    pq.push(10);
+    pq.push(30);
+    pq.push(50);
+    pq.push(70);
+    cout<< "priority queue (max-heap)\n";
+    while(!pq.empty()){
+        cout<<"top element"<<pq.top()<<endl;
+        pq.pop();
+        cout<<"Remaining size: "<<pq.size()<<"\n";
+    }
+    if(pq.empty()){
+        cout<<"empty priority queue"<<endl;
+    }
+
+}
